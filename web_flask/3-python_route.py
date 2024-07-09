f8536938ad1513199fdff9bab524a31114680bea
UmbraServus <ksprague0592@gmail.com>
2024-07-09 16:53:16 -0500

    didnt realize the @app.route needed to hae two blank spaces between the next route.

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,6 +21,8 @@ def c_text(text):
     return 'C {}'.format(text)
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+
+
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     text = text.replace('_', ' ')
